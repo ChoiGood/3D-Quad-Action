8fa74c800fe9d4994b6204d958fe316e78ce7c66
Ji Won Choi <jiwonchoi3794@gmail.com>
2023-09-09 20:57:13 +0900

    [골드메탈] 3D 쿼터뷰 액션게임 -플레이어 이동-

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();  // Animator 변수를 GetComponentInChildren() 으로 초기화.
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;  // normalized : 방향값이 1로 보저오딘 벡터.
+
+        /*  if(wDown)
+              transform.position += moveVec * speed * 0.3f * Time.deltaTime;
+          else
+              transform.position += moveVec * speed * Time.deltaTime;*/
+
+        // ==> 한줄로 쓰고 싶으면 ? 삼항연산자
+
+        transform.position += moveVec * speed *(wDown ? 0.3f : 1f) *  Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
