d99ca2227f273eee53a0599fedb76eb68fee56c3
Ji Won Choi <jiwonchoi3794@gmail.com>
2023-09-10 16:34:59 +0900

    [골드메탈] 3D 쿼터뷰 액션게임 -플레이어 점프와 회피-

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,23 +9,46 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
+    Rigidbody rigid;
     Animator anim;
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();  // Animator 변수를 GetComponentInChildren() 으로 초기화.
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;  // normalized : 방향값이 1로 보저오딘 벡터.
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;  // normalized : 방향값이 1로 보정된 벡터.
+
+        if (isDodge)
+            moveVec = dodgeVec;
 
         /*  if(wDown)
               transform.position += moveVec * speed * 0.3f * Time.deltaTime;
@@ -34,11 +57,54 @@ public class Player : MonoBehaviour
 
         // ==> 한줄로 쓰고 싶으면 ? 삼항연산자
 
-        transform.position += moveVec * speed *(wDown ? 0.3f : 1f) *  Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown && moveVec == Vector3.zero &&!isJump && !isDodge)   // bool 변수는 실행 조건으로 활용 ,,, 1. 움직임을 조건으로 추가해서 점프와 회피로 나누기.
+        {                                                                                             // 2. 액션 도중에 다른 액션이 실행되지 않도록 조건 추가.              
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)   // bool 변수는 실행 조건으로 활용 
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+
+            Invoke("DodgeOut", 0.6f); // Invoke() 함수로 시간차 함수 호출
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
